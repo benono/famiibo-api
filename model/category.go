557f35310e018d3a13b46665de149ac4841acc8c
BenOno <bentoki.1213@gmail.com>
2024-06-16 06:25:23 -0700

    Feat Add Order to Category

--- model/category.go
+++ model/category.go
@@ -4,6 +4,7 @@ import "time"
 
 type Category struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
+	Order     uint      `json:"order" gorm:"not null"`
 	Name      string    `json:"name" gorm:"not null"`
 	FamilyID  uint      `json:"family_id" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
